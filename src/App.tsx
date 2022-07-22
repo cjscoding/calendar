91e9a08829f7f19362f5f3fc180cf6c761bf21b7
최지수 <cjscoding1997@gmail.com>
2022-07-22 11:14:15 +0900

    add homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import store, { persistor } from "./store/configureStore";
+import store from "./store/configureStore";
 import Home from "./pages/home";
 
 function App() {
   return (
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/calendar" element={<Home />} />
         </Routes>
       </BrowserRouter>
-      {/* </PersistGate> */}
     </Provider>
   );
 }
